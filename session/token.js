a68c3188d116085ff23e9fddd98fd9fd5970f3d4
ZUKO-MD <132104247+Neggy5@users.noreply.github.com>
2026-07-16 00:15:39 +0100

    Add files via upload

---
+++ session/token.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN || '8645894383:AAFi1lcg9kwxoLbuTvTLi1wCGZGkHsQbIT0',
+  startupPassword: process.env.STARTUP_PASSWORD || 'empire'
+};
